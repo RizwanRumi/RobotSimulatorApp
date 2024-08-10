a6d2fde6a6fd5c9bef81bb3aa0a593f0b77f1a5d
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-08-10 21:53:30 +0200

    add interface and class for Move

---
+++ RobotSimulatorC#/RobotSimulatorTestApp/IMove.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSimulatorTestApp
+{
+    public interface IMove
+    {
+
+        public int Move(int position, int boundary);
+    }
+    public class MoveUp : IMove
+    {
+        public int Move(int position, int boundary)
+        {
+            int row = position;
+            if (row > 0) row--;
+            return row;
+        }
+    }
+
+    public class MoveDown : IMove
+    {
+        public int Move(int position, int boundary)
+        {
+            int row = position;
+            int maxRows = boundary;
+
+            if (row < maxRows - 1) row++;
+            return row;
+        }
+    }
+
+    public class MoveRight : IMove
+    {
+        public int Move(int position, int boundary)
+        {
+            int col = position;
+            int maxCols = boundary;
+
+            if (col < maxCols - 1) col++;
+            return col;
+        }
+    }
+
+    public class MoveLeft : IMove
+    {
+        public int Move(int position, int boundary)
+        {
+            int col = position;
+            if (col > 0) col--;
+            return col;
+        }
+    }
+}
