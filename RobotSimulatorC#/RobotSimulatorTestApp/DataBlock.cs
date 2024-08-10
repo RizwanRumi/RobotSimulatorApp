e0b3d241bfbe4e3669641d9d225020c164f56f78
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-08-10 17:53:49 +0200

    add datablock class

---
+++ RobotSimulatorC#/RobotSimulatorTestApp/DataBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSimulatorTestApp
+{
+    public enum Direction { N, E, S, W }
+
+    public class DataBlock
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+        public int CurrentRow { get; }
+        public int CurrentColumn { get; }
+        public Direction CurrentDirection { get; }
+        public string Commands { get; }
+
+        public DataBlock(int rows, int columns, int currentRow, int currentColumn, Direction direction, string commands)
+        {
+            this.Rows = rows;
+            this.Columns = columns;
+            this.CurrentRow = currentRow;
+            this.CurrentColumn = currentColumn;
+            this.CurrentDirection = direction;  
+            this.Commands = commands;
+        }
+    }
+}
