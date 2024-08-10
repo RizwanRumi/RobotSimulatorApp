6c935a2fe9430237aa4a81e3a7c545879359848f
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-08-10 17:54:34 +0200

    edit program class

--- RobotSimulatorC#/RobotSimulatorTestApp/Program.cs
+++ RobotSimulatorC#/RobotSimulatorTestApp/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RobotSimulatorTestApp;
+using System.Drawing;
+using System.Reflection.PortableExecutable;
+
+
+
+public class Program
+{    
+    static void Main(string[] args)
+    {        
+        string path = @"C:\Personal_project\RobotSimulatorC#\RobotSimulatorTestApp\files\input.txt";
+
+        var dataParser = new DataParser();
+        
+        try
+        {
+            if (File.Exists(path))
+            {
+                // Read the entire file content
+                string[] lines = File.ReadAllLines(path);
+
+                var dataBlocks = dataParser.Parse(lines);
+            }
+            else
+            {
+                Console.WriteLine("File directory path is Not available, please check!");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The process failed: {0}", e.ToString());
+        }
+
+
+       
+    }
+
+    
+}
+
+
+
+
+
