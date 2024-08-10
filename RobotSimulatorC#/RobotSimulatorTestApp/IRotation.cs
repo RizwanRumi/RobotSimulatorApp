a3bca1f53466c32f53f63980ce348ea44d7974f6
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-08-10 21:53:54 +0200

    add interface and class for Rotation

---
+++ RobotSimulatorC#/RobotSimulatorTestApp/IRotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSimulatorTestApp
+{
+    public interface IRotation
+    {
+        Direction Rotate(Direction direction);
+    }
+
+    public class RotateRight : IRotation
+    {
+        public Direction Rotate(Direction direction)
+        {
+            return (Direction)(((int)direction + 1) % 4);
+        }
+    }
+
+    public class RotateLeft : IRotation
+    {
+        public Direction Rotate(Direction direction)
+        {
+            return (Direction)(((int)direction + 3) % 4);
+        }
+    }
+}
