e7440a2133e033d9a8579271ca571377d67fb38d
RUMI\Rumi <rizwan.cse33@gmail.com>
2024-08-10 21:04:32 +0200

    edit program class

--- RobotSimulatorC#/RobotSimulatorTestApp/Program.cs
+++ RobotSimulatorC#/RobotSimulatorTestApp/Program.cs
@@ -20,6 +20,53 @@ public class Program
                 string[] lines = File.ReadAllLines(path);
 
                 var dataBlocks = dataParser.Parse(lines);
+
+                foreach (var block in dataBlocks)
+                {
+                    Console.WriteLine("\nInput:");
+                    Console.WriteLine($"{block.Rows} {block.Columns}");
+                    Console.WriteLine($"{block.CurrentRow}, {block.CurrentColumn}, {block.CurrentDirection}");
+                    Console.WriteLine($"{block.Commands}");
+                    Console.WriteLine();
+
+                    int rows = block.Rows;
+                    int cols = block.Columns;
+                    int currentRow = block.CurrentRow;
+                    int currentCol = block.CurrentColumn;
+                    Direction currentDirection = (Direction)Enum.Parse(typeof(Direction), value: Convert.ToString(block.CurrentDirection));
+
+                    RobotSimulator robotSimulator = new RobotSimulator();
+
+                   
+                    // Process commands
+                    string status = "";
+                    foreach (char command in block.Commands)
+                    {
+
+                        switch (command)
+                        {
+                            case 'M':
+                                robotSimulator.Move(ref currentRow, ref currentCol, currentDirection, rows, cols);
+                                status = "Position";
+                                break;
+                            case 'R':
+                                currentDirection = robotSimulator.RotateRight(currentDirection);
+                                status = "location";
+                                break;
+                            case 'L':
+                                currentDirection = robotSimulator.RotateLeft(currentDirection);
+                                status = "location";
+                                break;
+                        }
+
+                       Console.WriteLine($"Current {status}: {currentRow} {currentCol} {currentDirection}");
+                    }
+
+                    // Output the final position and direction
+                    Console.WriteLine($"Output : {currentRow} {currentCol} {currentDirection}");
+
+                    
+                }
             }
             else
             {
